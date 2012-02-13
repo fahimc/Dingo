582efbf069d288de22d05bdacac62d89047cae9b
fahimc <fahim@gmx.com>
2012-02-13 07:06:27 -0800

    fix layout issue on basic template

--- template/basic/src/BasicMain.js
+++ template/basic/src/BasicMain.js
@@ -12,13 +12,13 @@
 		//var nicEditButtons = document.getElementsByClassName('nicEdit-panelContain');
 		var cview = document.getElementById('createView');
 		var createLeftCol = document.getElementById('createLeftCol');
-		main.style.width = (stageWidth() - 200) + "px";
-		adminLabelCopy.style.width = (stageWidth() - 400) + "px";
-		createLeftCol.style.width = (stageWidth() - 400) + "px";
+		main.style.width = (stageWidth() - 220) + "px";
+		adminLabelCopy.style.width = (stageWidth() - 420) + "px";
+		createLeftCol.style.width = (stageWidth() - 420) + "px";
 		//nicEditButtons[0].style.width = (stageWidth() - 400) + "px";
 		//nicEdit[0].style.width = (stageWidth() - 400) + "px";
 		for(var i = 0; i < headers.length; i++) {
-			headers[i].style.width = (stageWidth() - 400) + "px";
+			headers[i].style.width = (stageWidth() - 420) + "px";
 		}
 		tinyMCE.init({
 			mode : "textareas",
